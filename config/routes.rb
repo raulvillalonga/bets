d6472d848f879146402dd61cb4387f99c695f290
Raul Villalonga <raulvillalonga@gmail.com>
2016-01-29 12:07:00 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+     authenticated :user do
+       root 'site#home', as: :authenticated_root
+     end
+     unauthenticated do
+       root 'devise/sessions#new', as: :unauthenticated_root
+     end
+   end
+
+end
